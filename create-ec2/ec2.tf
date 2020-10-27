4c6de75ada6ca93ac44e50cd2af3dbb51ee24b50
gautham990 <gautham990@gmail.com>
2020-10-27 21:59:19 +0800

    commit

--- create-ec2/ec2.tf
+++ create-ec2/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 #EC2 resource
-resource "aws_instance" "" {
+resource "aws_instance" "terraform-ec2" {
   ami = "ami-015a6758451df3cb9"
   instance_type = "t2.micro"
 }
