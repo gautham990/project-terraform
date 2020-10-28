502086470db1004349b30c66c24949359aa61a6d
gautham990 <gautham990@gmail.com>
2020-10-28 11:42:31 +0800

    commit

---
+++ create-ec2-vars/ec2-vars.tf
@@ -0,0 +1,7 @@
+#EC2 resource
+resource "aws_instance" "terraform-ec2-vars" {
+  ami = var.ami
+  instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-ec2-vars"
+  }
---
+++ create-ec2-vars/variable.tf
@@ -0,0 +1,6 @@
+variable "region" {
+  default = ap-southeast-1
+}
+variable "ami" {
+  default = ami-0d42c4e660c5c4135
+}
