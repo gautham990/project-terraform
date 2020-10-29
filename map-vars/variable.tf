9f0e8abd64c7e6b12d0409d5b88718020a2ac87e
gautham990 <gautham990@gmail.com>
2020-10-29 18:40:21 +0800

    commit

--- map-vars/variable.tf
+++ map-vars/variable.tf
@@ -1,8 +1,10 @@
 variable "ami"{
-  type = "map"
+  type = map
   default = {
     "ap-southeast-1" = "ami-02b6d9703a69265e9"
     "us-east-1" = "ami-0a782e324655d1cc0"
   }
 }
-variable "region" {}
+variable "region" {
+  default = "ap-southeast-1"
+}
