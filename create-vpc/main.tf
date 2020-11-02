df1fcd34f4040233bdcaa2bd0990b3714ca9566b
gautham990 <gautham990@gmail.com>
2020-11-02 15:58:40 +0800

    commit

--- create-vpc/main.tf
+++ create-vpc/main.tf
@@ -9,38 +9,38 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-variable "az-names" {
-  type = list
-  default = ["ap-southeast-1a","ap-southeast-1b","ap-southeast-1c"]
+variable "cidr" {
+  type = "map"
+  default = {
+    ap-southeast-1a = 10.0.1.0/24
+    ap-southeast-1b = 10.0.2.0/24
+    ap-southeast-1c = 10.0.3.0/24
+  }
 }
-variable "subnet-cidr" {
-  type = list
-  default = ["10.0.0.0","10.0.1.0","10.0.2.0"]
+variable "az" {
+  type = "list"
+  default = ["ap-southeast-1a","ap-southeast-1b","ap-southeast-1c"]
 }
 
 resource "aws_vpc" "prod-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-
   tags = {
     Name = "prod-vpc"
   }
 }
-
 resource "aws_internet_gateway" "prod-IG" {
   vpc_id = aws_vpc.prod-vpc.id
-
   tags = {
     Name = "prod-IG"
   }
 }
 resource "aws_subnet" "prod-subnet" {
   vpc_id     = aws_vpc.prod-vpc.id
-  cidr_block = var.subnet-cidr[count.index]/24
-  availability_zone = "var.az-names[count.index]"
-
+  availability_zone = var.az[count.index]
+  cidr_block = var.cidr[var.az]
   tags = {
-    Name = "var.az-names[count.index]"
+    Name = prod-subnet-var.az[count.index]
   }
   count = 3
 }
