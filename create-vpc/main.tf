41cdbdd0ad3028e0e548466427b095a6497640d0
gautham990 <gautham990@gmail.com>
2020-11-02 13:36:06 +0800

    commit

---
+++ create-vpc/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+variable "az-names" {
+  type = list
+  default = ["ap-southeast-1a","ap-southeast-1b","ap-southeast-1c"]
+}
+variable "subnet-cidr" {
+  type = list
+  default = ["10.0.0.0/24","10.0.1.0/24","10.0.2.0/24"]
+}
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "prod-IG" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "prod-IG"
+  }
+}
+resource "aws_subnet" "prod-subnet" {
+  vpc_id     = aws_vpc.prod-vpc.id
+  cidr_block = "var.subnet-cidr[count.index]"
+  availability_zone = "var.az-names[count.index]"
+
+  tags = {
+    Name = "var.az-names[count.index]"
+  }
+  count = 3
+}
+/*
+resource "aws_route_table" "prod-rt" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prod-IG.id
+  }
+}
+*/
