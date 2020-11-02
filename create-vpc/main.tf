05e3dfa43a609a1b5171215a7982865b666b873e
gautham990 <gautham990@gmail.com>
2020-11-02 16:02:01 +0800

    commit

--- create-vpc/main.tf
+++ create-vpc/main.tf
@@ -10,15 +10,13 @@ provider "aws" {
 }
 
 variable "cidr" {
-  type = "map"
   default = {
-    ap-southeast-1a = 10.0.1.0/24
-    ap-southeast-1b = 10.0.2.0/24
-    ap-southeast-1c = 10.0.3.0/24
+    "ap-southeast-1a" = "10.0.1.0/24"
+    "ap-southeast-1b" = "10.0.2.0/24"
+    "ap-southeast-1c" = "10.0.3.0/24"
   }
 }
 variable "az" {
-  type = "list"
   default = ["ap-southeast-1a","ap-southeast-1b","ap-southeast-1c"]
 }
 
