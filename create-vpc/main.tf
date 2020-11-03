6cc70b7ee29bfaa03613c3a2b131df852d0a7f50
gautham990 <gautham990@gmail.com>
2020-11-03 14:15:25 +0800

    commit

--- create-vpc/main.tf
+++ create-vpc/main.tf
@@ -67,7 +67,7 @@ resource "aws_security_group" "web-server" {
       from_port   = ingress.value
       to_port     = ingress.value
       protocol    = "tcp"
-      cidr_blocks = [0.0.0.0/0]
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
 
