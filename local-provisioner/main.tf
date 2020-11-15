8b989c9fc1dbbc9fd2767c888770ffe5c3550bfb
gautham990 <gautham990@gmail.com>
2020-11-15 08:42:28 +0800

    commit

---
+++ local-provisioner/main.tf
@@ -0,0 +1,15 @@
+locals {
+  common_tags = {
+    Name = "web-server"
+    Environment = "PROD"
+    Type = "frontend"
+  }
+}
+resource "aws_instance" "web-server" {
+  ami           = "ami-015a6758451df3cb9"
+  instance_type = "t2.micro"
+  tags = local.common_tags
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web-server.private_ip} >> private_IP.txt"
+  }
+}
