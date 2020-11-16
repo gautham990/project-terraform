7a365764e8a1632f194e45faa8f81b327d36d934
gautham990 <gautham990@gmail.com>
2020-11-16 16:25:58 +0800

    commit

---
+++ vpc-module/main.tf
@@ -0,0 +1,58 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.CIDR
+  instance_tenancy = "default"
+  tags = {
+    Name = var.VPC-name
+  }
+}
+resource "aws_internet_gateway" "IG" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = var.IG-name
+  }
+}
+data "aws_availability_zones" "AZ" {
+  all_availability_zones = true
+}
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  availability_zone = data.aws_availability_zones.AZ[count.index]
+  cidr_block = var.subnet_CIDR[count.index]
+  tags = {
+    Name = var.subnet-name[count.index]
+  }
+  count = data.aws_availability_zones.AZ.count
+}
+resource "aws_route" "main-RT" {
+  route_table_id = aws_vpc.vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.IG.id
+}
+resource "aws_route_table_association" "rt-association" {
+  route_table_id = aws_vpc.vpc.main_route_table_id
+  subnet_id      = aws_subnet.subnet[count.index].id
+  count = data.aws_availability_zones.AZ.count
+}
+resource "aws_security_group" "SG" {
+  name        = lookup(var.SG-name,Name )
+  description = lookup(var.SG-name,Description )
+  vpc_id      = aws_vpc.vpc.id
+  dynamic "ingress" {
+    for_each = var.sec-groups-ports
+    content {
+      from_port   = ingress.key
+      to_port     = ingress.key
+      protocol    = "tcp"
+      cidr_blocks = ingress.value
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = lookup(var.SG-name,Name)
+  }
+}
---
+++ vpc-module/variables.tf
@@ -0,0 +1,36 @@
+variable "CIDR" {
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+variable "VPC-name" {
+  type = string
+  default = DEV-VPC
+}
+variable "IG-name" {
+  type = string
+  default = DEV-IG
+}
+variable "subnet_CIDR" {
+  type = list(data.aws_availability_zones.AZ.count)
+  default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+}
+variable "subnet-name" {
+  type = list(string)
+  default = ["DEV-sub-1","DEV-sub-2","DEV-sub-3"]
+}
+variable "SG-name" {
+  type = map
+  default = {
+    Name = "DEV-SG"
+    Description = "This is default SG created with the module"
+  }
+}
+variable "sec-groups-ports" {
+  description = "Allowed ports"
+  type        = map
+  default     = {
+    "443" = ["0.0.0.0/0"]
+    "22"  = ["0.0.0.0/0"]
+    "80"  = ["0.0.0.0/0"]
+  }
+}
