07493474b2844ab655d612a05c53b9bbe21701c5
gautham990 <gautham990@gmail.com>
2020-11-17 08:44:21 +0800

    commit

--- vpc-module/variables.tf
+++ vpc-module/variables.tf
@@ -11,7 +11,7 @@ variable "IG-name" {
   default = "DEV-IG"
 }
 variable "subnet_CIDR" {
-  type = list(data.aws_availability_zones.AZ.count)
+  type = list(string)
   default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
 }
 variable "subnet-name" {
