eefa359e9496f2d15b36b8b46592cab98e3fd93d
gautham990 <gautham990@gmail.com>
2020-11-24 20:25:17 +0800

    commit

---
+++ s3/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "project-devops-241120"
+  acl    = "private"
+
+  tags = {
+    Name        = "test-bucket"
+    Environment = "Dev"
+    Terraform = "yes"
+  }
+}
