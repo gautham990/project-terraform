8f8457312b7f83916746d45cbbf744db2d38da29
gautham990 <gautham990@gmail.com>
2020-11-24 13:09:35 +0800

    commit

--- vpc-ec2-module/main.tf
+++ vpc-ec2-module/main.tf
@@ -35,13 +35,6 @@ resource "aws_security_group" "web-server" {
     Name = "allow-ssh"
   }
 }
-output "ID-sg" {
-  value = aws_security_group.web-server.id
-}
-output "ID-subnet" {
-  value = module.vpc.public_subnets[0]
-}
-
 module "ec2_cluster" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
