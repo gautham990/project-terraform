3f1f324d839da7b7a6c173fb6d89916f80d9bca0
gautham990 <gautham990@gmail.com>
2020-11-24 20:38:20 +0800

    commit

--- s3/main.tf
+++ s3/main.tf
@@ -1,7 +1,23 @@
 resource "aws_s3_bucket" "test-bucket" {
   bucket = "project-devops-241120"
  # acl    = "private"
-
+  policy = <<EOF
+[{
+  "Id": "Policy1606205384131",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1606205372733",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": ${aws_s3_bucket.test-bucket.arn},
+      "Principal": "*"
+    }
+  ]
+}]
+EOF
   tags = {
     Name        = "test-bucket"
     Environment = "Dev"
