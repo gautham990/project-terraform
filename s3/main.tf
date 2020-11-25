8a7af4575fb592ac0683e2e031c90bbee03fa411
gautham990 <gautham990@gmail.com>
2020-11-25 09:10:06 +0800

    commit

--- s3/main.tf
+++ s3/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = var.bucket-name
   acl    = "private"
-  policy = <<EOF
+ /* policy = <<EOF
 {
   "Id": "Policy1606205384131",
   "Version": "2012-10-17",
@@ -18,6 +18,7 @@ resource "aws_s3_bucket" "s3-bucket" {
   ]
 }
 EOF
+*/
   website {
     index_document = "index.html"
     error_document = "error.html"
