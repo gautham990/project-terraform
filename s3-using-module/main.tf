69b5f20b155cc5376b3e8d4a5a03c6badaa27690
gautham990 <gautham990@gmail.com>
2020-11-26 14:15:46 +0800

    commit

--- s3-using-module/main.tf
+++ s3-using-module/main.tf
@@ -1,8 +1,10 @@
 module "s3-module" {
-  source = "../s3-module"
+  source = "app.terraform.io/project-devops/s3-website/aws"
+  version = "1.0.0"
   bucket-name = "project-devops-251120"
   tags = {
     env = "dev"
     terraform = "yes"
   }
 }
+
