e0a0b0c7d755d4361549fa590f2bfb01d961cc04
gautham990 <gautham990@gmail.com>
2020-11-26 13:34:50 +0800

    commit

--- s3-module/outputs.tf
+++ s3-module/outputs.tf
@@ -1,6 +1,8 @@
 output "bucket-arn" {
+  description = "ARN of the bucket"
   value = aws_s3_bucket.s3-bucket.arn
 }
 output "bucket-endpoint" {
+  description = "Website endpoint"
   value = aws_s3_bucket.s3-bucket.website_endpoint
 }
--- s3-module/variables.tf
+++ s3-module/variables.tf
@@ -1,6 +1,8 @@
 variable "bucket-name" {
+  description = "Unique name of the bucket"
   type = string
 }
 variable "tags" {
+  description = "Tags for the bucket"
   type = map(string)
 }
