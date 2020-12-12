ab831e7205ce5fc5b8c0c118e2266928f48a504a
gautham990 <gautham990@gmail.com>
2020-12-12 20:52:01 +0800

    commit

--- test-config/main.tf
+++ test-config/main.tf
@@ -18,6 +18,6 @@ resource "aws_subnet" "subnet" {
   cidr_block = var.subnet-cidr[count.index]
   vpc_id = aws_vpc.VPC.id
   availability_zone = data.aws_availability_zones.AZ.names[count.index]
-  count = length(tomap(data.aws_availability_zones.AZ.names))
+  count = length(data.aws_availability_zones.AZ.names)
 }
 
